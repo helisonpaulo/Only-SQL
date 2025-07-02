5010e83e9d75f231da17ef3911e85d63bc189175
Helison Paulo <helisonfabio@gmail.com>
2025-07-02 17:07:39 -0300

    Update consulta.sql

--- consulta.sql
+++ consulta.sql
@@ -1,4 +1,5 @@
--- Criando uma CTE para agrupar as primeiras informações e novas colunas
+-- 1ª CTE: data limite que o pedido deve ser entregue
+-- 2ª CTE: data em que foi entregue os pedidos
 with data_limite as(
     SELECT
         order_id,
@@ -8,7 +9,8 @@ with data_limite as(
 data_entrega AS(
     SELECT
         order_id,
-        STRFTIME('%Y-%m-%d', order_delivered_customer_date) AS data_entrega
+        STRFTIME('%Y-%m-%d', order_delivered_customer_date) AS data_entrega,
+        STRFTIME('%Y-%m', order_delivered_customer_date) AS mes_entrega -- Mês para a partição
     FROM tb_orders
 )
 
@@ -16,10 +18,11 @@ SELECT
     t1.order_id,
     t1.data_limite,
     t2.data_entrega,
-    CAST(JULIANDAY(t2.data_entrega) - JULIANDAY(t1.data_limite) AS INT64) AS dias_atraso
+    CAST(JULIANDAY(t2.data_entrega) - JULIANDAY(t1.data_limite) AS INT64) AS dias_atraso,
+    -- Classificando os pedidos por atraso (do maior para o menor) DENTRO DE CADA MÊS
+    RANK() OVER (PARTITION BY t2.mes_entrega ORDER BY CAST(JULIANDAY(t2.data_entrega) - JULIANDAY(t1.data_limite) AS INT64) DESC) AS rank_atraso_mensal
 FROM data_limite AS t1
 LEFT JOIN data_entrega AS t2
     ON t1.order_id = t2.order_id
 WHERE data_limite < data_entrega
-ORDER BY dias_atraso DESC
--- Colocado em ordem da maior quantidade de dias de atraso
+ORDER BY mes_entrega DESC; -- Ordenado pelo mês mais recente
